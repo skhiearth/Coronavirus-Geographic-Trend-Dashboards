4552363e268a372ac951c64b60f979f441b44510
Utkarsh Sharma <skhiearth@gmail.com>
2020-03-10 00:52:47 +0530

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -8,41 +8,40 @@ from bokeh.embed import components
 app = Flask(__name__)
 
 
-confirmed_raw_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
-                    "/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
-deaths_raw_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
-                 "/csse_covid_19_time_series/time_series_19-covid-Deaths.csv "
-recovered_raw_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
-                    "/csse_covid_19_time_series/time_series_19-covid-Recovered.csv "
-
-confirmed_df_raw = pd.read_csv(confirmed_raw_url)
-deaths_df_raw = pd.read_csv(deaths_raw_url)
-recovered_df_raw = pd.read_csv(recovered_raw_url)
-
-latest_date = confirmed_df_raw.columns[-1]
-
-confirmed_df_raw = confirmed_df_raw.drop(['Province/State', 'Lat', 'Long'], axis=1)
-total_confirmed = confirmed_df_raw.groupby('Country/Region', as_index=False).agg(sum).sort_values(latest_date,
-                                                                                                  ascending=False)
-total_confirmed = pd.melt(total_confirmed, id_vars='Country/Region', var_name='Dates', value_name='Count')
-total_confirmed['Dates'] = pd.to_datetime(total_confirmed['Dates'])
-
-deaths_df_raw = deaths_df_raw.drop(['Province/State', 'Lat', 'Long'], axis=1)
-total_deaths = deaths_df_raw.groupby('Country/Region', as_index=False).agg(sum).sort_values(latest_date,
-                                                                                            ascending=False)
-total_deaths = pd.melt(total_deaths, id_vars='Country/Region', var_name='Dates', value_name='Count')
-total_deaths['Dates'] = pd.to_datetime(total_deaths['Dates'])
-
-recovered_df_raw = recovered_df_raw.drop(['Province/State', 'Lat', 'Long'], axis=1)
-total_recovered = recovered_df_raw.groupby('Country/Region', as_index=False).agg(sum).sort_values(latest_date,
-                                                                                                  ascending=False)
-total_recovered = pd.melt(total_recovered, id_vars='Country/Region', var_name='Dates', value_name='Count')
-total_recovered['Dates'] = pd.to_datetime(total_recovered['Dates'])
-
-country_names = total_recovered['Country/Region'].unique()
-
-
 def plot_data(_country):
+    confirmed_raw_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
+                        "/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv "
+    deaths_raw_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
+                     "/csse_covid_19_time_series/time_series_19-covid-Deaths.csv "
+    recovered_raw_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
+                        "/csse_covid_19_time_series/time_series_19-covid-Recovered.csv "
+
+    confirmed_df_raw = pd.read_csv(confirmed_raw_url)
+    deaths_df_raw = pd.read_csv(deaths_raw_url)
+    recovered_df_raw = pd.read_csv(recovered_raw_url)
+
+    latest_date = confirmed_df_raw.columns[-1]
+
+    confirmed_df_raw = confirmed_df_raw.drop(['Province/State', 'Lat', 'Long'], axis=1)
+    total_confirmed = confirmed_df_raw.groupby('Country/Region', as_index=False).agg(sum).sort_values(latest_date,
+                                                                                                      ascending=False)
+    total_confirmed = pd.melt(total_confirmed, id_vars='Country/Region', var_name='Dates', value_name='Count')
+    total_confirmed['Dates'] = pd.to_datetime(total_confirmed['Dates'])
+
+    deaths_df_raw = deaths_df_raw.drop(['Province/State', 'Lat', 'Long'], axis=1)
+    total_deaths = deaths_df_raw.groupby('Country/Region', as_index=False).agg(sum).sort_values(latest_date,
+                                                                                                ascending=False)
+    total_deaths = pd.melt(total_deaths, id_vars='Country/Region', var_name='Dates', value_name='Count')
+    total_deaths['Dates'] = pd.to_datetime(total_deaths['Dates'])
+
+    recovered_df_raw = recovered_df_raw.drop(['Province/State', 'Lat', 'Long'], axis=1)
+    total_recovered = recovered_df_raw.groupby('Country/Region', as_index=False).agg(sum).sort_values(latest_date,
+                                                                                                      ascending=False)
+    total_recovered = pd.melt(total_recovered, id_vars='Country/Region', var_name='Dates', value_name='Count')
+    total_recovered['Dates'] = pd.to_datetime(total_recovered['Dates'])
+
+    country_names = total_recovered['Country/Region'].unique()
+
     mask_confirmed = total_confirmed['Country/Region'] == _country
     _confirmed = ColumnDataSource(total_confirmed[mask_confirmed])
 
@@ -64,7 +63,7 @@ def plot_data(_country):
     p.add_tools(hover)
     p.legend.location = 'top_left'
 
-    return p
+    return p, country_names
 
 
 @app.route('/')
@@ -75,13 +74,12 @@ def index():
         current_country_name = "Mainland China"
 
     # Create the plot
-    plot = plot_data(current_country_name)
+    plot, name = plot_data(current_country_name)
 
     # Embed plot into HTML via Flask Render
     script, div = components(plot)
-    print(div)
     return render_template("index.html", script_plot=script, div_plot=div,
-                           country_names=country_names, current_country_name=current_country_name)
+                           country_names=name, current_country_name=current_country_name)
 
 
 if __name__ == '__main__':
